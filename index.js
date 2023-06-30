93543790c2a7460f234039fe82fc24ad1fb5c471
Tony Ivanov <telamohn@pm.me>
2023-06-30 02:16:42 +0200

    Well this feels and looks better than previous attempts

--- index.js
+++ index.js
@@ -1,1 +1,226 @@
 // SPDX-License-Identifier: AGPL-3.0-or-later
+import { Feed } from 'picofeed'
+import { decode, encode } from 'cbor-x'
+
+export const [
+  DMAP,
+  DSET,
+  DNUM
+  // DSTR
+] = [0, 1, 2, 3]
+// For GC@expiredAt; Ops need to be reversible
+// DNUM OPS
+export const OP_INC = 10
+export const OP_DEC = ~OP_INC
+// DSET
+export const OP_ADD = 11
+export const OP_DEL = ~OP_ADD
+// DMAP
+export const OP_SET = 12
+export const OP_UST = ~OP_SET
+
+/**
+ * @typedef {number} PUID
+ * @returns {PUID} A 24bit random number
+ */
+export function mkPUID () {
+  const b = new Uint8Array(4)
+  globalThis.crypto.getRandomValues(b)
+  return (b[2] << 16) | (b[1] << 8) | b[0]
+}
+
+export class CRDTNumber {
+  inc = 0
+  dec = 0
+  constructor (v = 0) { this.value = v }
+  get value () { return this.inc - this.dec }
+  set value (n) {
+    const d = n - (this.inc - this.dec)
+    if (n > this.value) this.inc += d
+    else this.dec += d
+  }
+}
+
+export class CRDTArray {
+  vals = []
+  /** @type {Set<PUID>} */
+  pids = new Set()
+  /** @type {Set<PUID>} */
+  tomb = new Set()
+  constructor (values = []) {
+    if (!Array.isArray(values)) throw new Error('ArrayExpected')
+    for (const v of values) this.push(mkPUID(), v)
+  }
+
+  /**
+   * @param {PUID} pid
+   * @param {el?} Any
+   */
+  push (pid, el) {
+    if (this.pids.has(pid)) {
+      if (this.tomb.delete(pid)) return undefined
+      else throw new Error('ObjectAlreadyExists')
+    }
+    this.pids.add(pid)
+    this.vals.push(el)
+    // TODO: return this.length
+  }
+
+  delete (pid) { this.tomb.add(pid) }
+
+  get value () {
+    const lut = Array.from(this.pids)
+    return this.vals.filter((_, i) => !this.tomb.has(lut[i]))
+  }
+
+  get length () { return this.value.length }
+  // [Symbol.iterator] () { }
+}
+
+export class Layer {
+  date = Date.now()
+  ops = [] // <-- yes { p: 'x', op: 'inc', a0: 3 }
+  signature = null // <-- when set, mutations frozen.
+  // expiresAt = (...) => boolean
+
+  /**
+   * @param {string} path Location in root DMAP
+   * @param {number} type Expected DObject Type
+   * @param {number} op Operation ID
+   * @param {any} a1 Operation Argument1
+   * @param {any} a1 Operation Argument2
+   */
+  instr (path, type, op, a1, a2 = null) {
+    if (a2 !== null) this.ops.push([path, type, op, a1, a2])
+    else this.ops.push([path, type, op, a1])
+  }
+
+  reverse () {
+    const l = new Layer()
+    l.ops = this.ops.map(i => {
+      const r = [...i]
+      r[2] = ~r[2] // All inverse ops are bitflip defined
+      return r
+    }).reverse()
+    return l
+  }
+
+  apply (o) {
+    for (const [path, type, op, arg1, arg2] of this.ops) {
+      let target = o
+      const segments = path.split('.')
+      while (target && segments.length > 1) {
+        const cd = segments.shift()
+        target = target[cd]
+      }
+      const prop = segments.shift()
+      switch (type) {
+        case DNUM:
+          target[prop] = target[prop] || new CRDTNumber()
+          if (!(target[prop] instanceof CRDTNumber)) throw new Error('PropIsNotANumber')
+          if (op === OP_INC) target[prop].inc += arg1
+          else if (op === OP_DEC) target[prop].dec += arg1
+          else throw new Error('DNUM:InvalidInstruction:' + op)
+          break
+        case DSET:
+          target[prop] = target[prop] || new CRDTArray()
+          if (!(target[prop] instanceof CRDTArray)) throw new Error('PropIsNotArray')
+          if (op === OP_ADD) target[prop].push(arg1, arg2)
+          else if (op === OP_DEL) target[prop].delete(arg1)
+          else throw new Error('DSET:InvalidInstruction:' + op)
+          break
+        case DMAP:
+          target[prop] = target[prop] || {}
+          if (op === OP_SET) target[prop][arg1] = arg2
+          else if (op === OP_UST) delete target[prop][arg1]
+          else throw new Error('DMAP:InvalidInstruction:' + op)
+          break
+        default:
+          throw new Error('UnknownType:' + type)
+      }
+    }
+  }
+
+  toFeed (sk, branch = new Feed()) {
+    branch.append(encode({ d: this.date, i: this.ops }), sk)
+    this.signature = branch.last.sig
+    // Object.freeze(this) // ?
+    return branch.last
+  }
+
+  static from (block) {
+    const j = decode(block.body)
+    const l = new Layer()
+    l.date = j.d
+    l.ops = Object.freeze(j.i)
+    l.signature = block.sig
+    return Object.freeze(l)
+  }
+}
+
+export class CPU {
+  layers = []
+  state = {}
+
+  /**
+   * Creates a new PicoCPU
+   * @param {Repo} repo A pico-repository
+   */
+  constructor (repo) {
+    this.repo = repo
+    this._sk = Feed.signPair().sk
+  }
+
+  async restore () {
+    // TODO: load state from this.repo
+  }
+
+  /**
+   * Creates a layer that
+   * that alters state
+   * @param {(state: Layer) => Promise<void>} callback Interface to mutate state
+   * @return Feed a feed containing the binary layer.
+   */
+  async mutate (callback) {
+    const layer = new Layer()
+    // const screen = this._proxyFor('/', layer)
+    await callback(layer)
+    return this.merge(layer.toFeed(this._sk))
+  }
+
+  /**
+   * @returns {Promise<Feed>} returns a feed with applied blocks
+   */
+  async merge (feed) { // Equals CPU-RUN/STEP
+    feed = Feed.from(feed)
+    for (const block of feed.blocks) {
+      const layer = Layer.from(block)
+      layer.apply(this.state) // Naive approach
+      this.layers.push(layer)
+    }
+    return feed
+  }
+
+  /** @type {path: string, writable: Layer} */
+  /*
+  _proxyFor (path, writable) {
+    const segments = path.replace(/^\//, '').split('/')
+    let target = this.state
+    while (target && segments.length) {
+      const s = segments.shift()
+      if (s.length) target = target[s]
+    }
+    if (!target) throw new Error('TargetNotFound')
+
+    return new Proxy(target, {
+      get (target, prop) {
+        debugger
+      },
+      set (target, prop, value) {
+        if (!writable) return false
+        debugger
+      }
+    })
+  }
+  */
+}
--- test.js
+++ test.js
@@ -1,167 +1,26 @@
-import test from 'tape'
-import { Feed } from 'picofeed'
 import { Repo } from 'picorepo'
+import test from 'tape'
 import { MemoryLevel } from 'memory-level'
+import {
+  CPU,
+  DNUM,
+  OP_INC,
+  OP_DEC,
+  DSET,
+  OP_ADD,
+  OP_DEL,
+  mkPUID,
+  Layer
+} from './index.js'
+import { webcrypto } from 'node:crypto'
+if (!globalThis.crypto) globalThis.crypto = webcrypto
+
 const mkRepo = () => new Repo(new MemoryLevel('cpu.lvl', {
   keyEncoding: 'buffer',
   valueEncoding: 'buffer'
 }))
-test('desc', async t => {
-  const cpu = new PicoCPU()
 
-  cpu.defineNumber('x', 3) // Or maybe 'increment'
-  await cpu.restore()
-  t.equal(cpu.state.x, 3)
-
-  const stage = cpu.createStage()
-  stage.update('x', 7)
-  stage.inspect()
-  const { sk } = Feed.signPair()
-  const forkA = new Feed()
-  const patch = await stage.commit(forkA, sk)
-  t.equal(cpu.state.x, 3)
-  patch.inspect()
-})
-
-test.skip('list when depression hits', async t => {
-  // Create CPU when everything feels meaningless
-  const cpu = new PicoCPU() // It's not designed to work.
-  const friends = cpu.defineList('friends') // greater than zero but what's the point?
-  cpu.defineList('bitches') // Tinder sux, gf sux good but gf is gf.
-
-  // Where is the magnetic tape supposed to go???
-  // there's like My picofeed where shit might get recorded
-  // or like a shitty game.
-  // FUUUUK I Need to ventilate. i've been holding my shit in for
-  // so long. SOOO FUCKING long. LOVE SUCKS
-  // LOVE SUCKS SO FUCKING MUCH
-})
-
-test.skip('instruction set approach', async t => {
-  // define a model, set script boundaries
-  // a single bucket as fixture
-  const bucket = [
-    {
-      text: 'World Peace Please',
-      date: 0,
-      ttl: 65, // Highlevel GC hint. 60 mins
-      bumps: 0 // n-likes/bumps
-    }
-  ]
-  const cpu = new PicoCPU()
-  await cpu.restore()
-
-  const stage = cpu.createStage()
-  stage.defineNumber('x', 3)
-  stage.incNumber('x', 9)
-  const { sk: skA } = Feed.signPair()
-  const feed = await stage.commit(skA)
-  feed.inspect()
-})
-
-class PicoCPU {
-  vars = {}
-  // constructor (ldb) {}
-  defineNumber (name, inititalValue = 0) {
-    this.vars[name] = new CRDTNumber(inititalValue)
-  }
-
-  get state () {
-    const snapshot = {}
-    for (const key in this.vars) snapshot[key] = this.vars[key].value
-    return snapshot
-  }
-
-  createStage () {
-    return new StagingArea(this)
-  }
-
-  async restore () { /* TODO: restore state from ldb */ }
-
-  async run (feed) {
-    console.log('CPU:run')
-    feed.inspect()
-  }
-}
-
-class CRDTNumber {
-  static TYPE = 0
-  increment = 0
-  decrement = 0
-  // multiplier = 1
-  // divisor = 1
-  constructor (inititalValue = 0) {
-    if (inititalValue < 0) this.decrement = -inititalValue
-    else this.increment = inititalValue
-  }
-
-  get value () {
-    return (this.increment - this.decrement) //  * this.multiplier / this.divisor
-  }
-
-  clone () {
-    const c = new CRDTNumber()
-    c.increment = this.increment
-    c.decrement = this.decrement
-    return c
-  }
-
-  pack () {
-    return [this.increment, this.decrement]
-  }
-
-  static unpack (arr) {
-    const [i, d] = arr
-    const c = new CRDTNumber()
-    c.increment = i
-    c.decrement = d
-    return c
-  }
-}
-
-class StagingArea {
-  vars = {}
-  date = Date.now()
-  status = 'pending'
-
-  constructor (root) {
-    this.root = root
-  }
-
-  update (name, value) {
-    let vr = this.vars[name]
-    if (!vr && !this.root.vars[name]) throw new Error(`No such variable: ${name}`)
-    else if (!vr) vr = this.vars[name] = this.root.vars[name].clone()
-
-    if (vr instanceof CRDTNumber) {
-      if (!Number.isFinite(value)) throw new Error(`Expected number, found: ${value}`)
-      if (vr.value < value) vr.increment += value - vr.value
-      else if (vr.value > value) vr.decrement += value - vr.value
-    }
-  }
-
-  async commit (feed, secret) {
-    const body = { d: Date.now() }
-    for (const key in this.vars) {
-      const vr = this.vars[key]
-      body[key] = [vr.constructor.TYPE, vr.pack()]
-    }
-    console.log(JSON.stringify(body))
-    feed.append(JSON.stringify(body), secret) // TODO: mpacker
-    await this.root.run(feed)
-    this.status = 'merged'
-  }
-
-  inspect () {
-    let str = `Patch ${this.date} [${this.status}]\n`
-    for (const key in this.vars) {
-      str += `V: ${this.root.vars[key].value} => ${this.vars[key].value}`
-    }
-    console.log(str)
-  }
-}
-
-test.only('Last 2023Q2 Attempt', async t => {
+test('Last 2023Q2 Attempt', async t => {
   const cpuA = new CPU(mkRepo(), {
     root: {} // Auto new DMap()
   })
@@ -179,95 +38,32 @@ test.only('Last 2023Q2 Attempt', async t => {
     // t.equal(state.x, 5)
 
     // Instruction vocabulary
-    layer.instr('x:n', 'inc', 1)
-    layer.instr('x:n', 'inc', 6)
-    layer.instr('x:n', 'dec', 1)
-    layer.instr('peers:a', 'push', 'alice')
-    layer.instr('peers:a', 'push', 'bob')
-    layer.instr('peers:a', 'push', 'charlie')
+    state.instr('x', DNUM, OP_INC, 1)
+    state.instr('x', DNUM, OP_INC, 6)
+    state.instr('x', DNUM, OP_DEC, 2)
+    // Array/UnorderedSet
+
+    state.instr('peers', DSET, OP_ADD, mkPUID(), 'alice')
+    state.instr('peers', DSET, OP_ADD, mkPUID(), 'bob')
+    const cid = mkPUID()
+    state.instr('peers', DSET, OP_ADD, cid, 'charlie')
+    state.instr('peers', DSET, OP_DEL, cid)
+    // state.instr('peers.3', DMAP, 'set', 'power', 4)
+    // state.instr('peers.3.speed', DNUM, 'inc', 10)
   })
-  t.equal(cpuA.get('x'), 2, 'Readable outside of mutation')
+  // console.log(cpuA.state)
+  t.equal(cpuA.state.x.value, 5, 'X was altered')
+  t.deepEqual(cpuA.state.peers.value, ['alice', 'bob'])
+
   const merged = await cpuB.merge(layer)
-  t.equal(merged, 1, 'Layers merged')
-  t.equal(cpuB.get('x'), 2, 'State replicated')
+  t.equal(merged.length, 1, 'Layers merged')
+  t.equal(cpuB.state.x.value, 5, 'X was altered')
+  t.deepEqual(cpuB.state.peers.value, ['alice', 'bob'])
+
+  const inv = Layer.from(merged.last).reverse()
+  // console.log('Inverse OPS', inv.ops)
+  inv.apply(cpuA.state)
+  t.equal(cpuA.state.x.value, 0, 'x is 0')
+  t.deepEqual(cpuA.state.peers.value, [], 'peers emptied')
 })
-
-/** @typedef {{ root: object }} PicoCPUOptions */
-class CPU {
-  layers = []
-  state = {}
-  /**
-   * Creates a new PicoCPU
-   * @param {Repo} repo A pico-repository
-   * @param {PicoCPUOptions} opts
-    */
-  constructor (repo) {
-    this.repo = repo
-  }
-
-  async restore () {
-    // TODO: load state from this.repo
-  }
-
-  /**
-   * Creates a layer that
-   * that alters state
-   * @param {async (state: Layer) => void} callback Interface to mutate state
-   * @return Feed a feed containing the binary layer.
-   */
-  async mutate (callback) {
-    const layer = new Layer()
-    const screen = this._proxyFor('/', layer)
-    await callback(screen)
-    return layer.toFeed()
-  }
-
-  /** @type {path: string, writable: Layer} */
-  _proxyFor (path, writable) {
-    const segments = path.replace(/^\//, '').split('/')
-    let target = this.state
-    while (target && segments.length) {
-      const s = segments.shift()
-      if (s.length) target = target[s]
-    }
-    if (!target) throw new Error('TargetNotFound')
-
-    return new Proxy(target, {
-      get (target, prop) {
-        debugger
-      },
-      set (target, prop, value) {
-        if (!writable) return false
-        debugger
-      }
-    })
-  }
-}
-
-class Layer {
-  mutations = [] // <-- yes { p: 'x', op: 'inc', a0: 3 }
-  signature = null // <-- when set, mutations frozen.
-}
-
-class DObj {
-  get value () { return this._value }
-}
-
-class DMap extends DObj {
-  _value = new Map()
-  constructor (defaultValue = {}) {
-    super()
-    for (const key in defaultValue) this._value.put(key, defaultValue[key])
-  }
-
-  /**
-   * Returns value for key
-   * @param {string} key
-   * @param {'number'|'map'|'set'} defaultType the expected
-   * @param {any} defaultValue the DObject value return when key dosen't exist
-   */
-  get (key, defaultType, defaultValue) {
-    if (defaultType && this._value.has(key)) this._value.put(key, decentralize(defaultType, defaultValue))
-    this._value.get(key)
-  }
-}
+// ---------- GPT REFINEMENT WORKBENCH
